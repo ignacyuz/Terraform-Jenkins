cc16bd86b04d89b096f74a59b39aed6f7948d5ca
ignacyuz <90883588+ignacyuz@users.noreply.github.com>
2024-10-20 01:22:36 -0600

    update to ami id

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+resource "aws_instance" "nacho-ami" {
+  ami           = "ami-06b21ccaeff8cd686"
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "nacho-tf-instance"
   }
 }
